e1ef8fdf3f01104afa075fd843cd6bd34abda308
Alexander Bessonov <bessonovUSA@gmail.com>
2022-10-09 21:09:43 -0500

    Initial commit

---
+++ src/test/java/com/example/pizzacloudproject/PizzaCloudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pizzacloudproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaCloudProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
